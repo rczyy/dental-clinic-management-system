0aebc6eae62955be7667f317067ea3cd30d59014
rcmercado81@gmail.com <rczyy>
2023-02-10 01:21:10 +0800

    Updated color theme

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -10,32 +10,34 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [{
-      lightTheme: {
-        "primary": "#2d9cdb",
-        "secondary": "#9ae6b4",
-        "accent": "#feb2b2",
-        "neutral": "#a0aec0",
-        "base-100": "#f7fafc",
-        "info": "#2d9cdb",
-        "success": "#9ae6b4",
-        "warning": "#ffedb3",
-        "error": "#feb2b2"
-      }
-    },
-    {
-      darkTheme: {
-        "primary": "#2d9cdb",
-        "secondary": "#2f855a",
-        "accent": "#feb2b2",
-        "neutral": "#a0aec0",
-        "base-100": "#1a202c",
-        "info": "#2d9cdb",
-        "success": "#9ae6b4",
-        "warning": "#ffedb3",
-        "error": "#feb2b2",
-      }
-    }
+    themes: [
+      {
+        lightTheme: {
+          primary: "#2d9cdb",
+          secondary: "#9ae6b4",
+          accent: "#dc2626",
+          neutral: "#a0aec0",
+          "base-100": "#f7fafc",
+          "base-300": "#ffffff",
+          info: "#2d9cdb",
+          success: "#9ae6b4",
+          warning: "#ffedb3",
+          error: "#dc2626",
+        },
+      },
+      {
+        darkTheme: {
+          primary: "#2d9cdb",
+          secondary: "#2f855a",
+          accent: "#dc2626",
+          neutral: "#a0aec0",
+          "base-100": "#1a202c",
+          info: "#2d9cdb",
+          success: "#9ae6b4",
+          warning: "#ffedb3",
+          error: "#dc2626",
+        },
+      },
     ],
   },
 };
