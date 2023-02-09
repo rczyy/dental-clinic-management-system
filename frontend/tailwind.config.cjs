0eab19e76b0d3f320c1e6c3d2fdf18fff777a29a
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2023-02-09 15:02:56 +0800

    Added daisyui, react icons, font

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        work: ['Work Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [require("daisyui")],
-};
+}
