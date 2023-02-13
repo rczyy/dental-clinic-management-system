99627065803bc29563f9022ee80061cb2580b122
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2023-02-13 16:35:33 +0800

    Minor sidebar changes

--- frontend/src/components/AdminSideBar.tsx
+++ frontend/src/components/AdminSideBar.tsx
@@ -1,7 +1,8 @@
 import { BiUserCheck } from "react-icons/bi";
 import { VscNote } from "react-icons/vsc";
-import { CgProfile, CgUserList, CgUserAdd, CgNotes } from "react-icons/cg";
+import { CgProfile, CgUserList, CgUserAdd } from "react-icons/cg";
 import { IconType } from "react-icons/lib";
+import { Link } from "react-router-dom";
 
 type SideBarItemProps = {
   name: string;
@@ -10,9 +11,9 @@ type SideBarItemProps = {
 
 const AdminSideBar = () => {
   return (
-    <div className="flex flex-col justify-between fixed h-screen w-fit border-r border-base-200 pt-[5rem]">
+    <div className="flex flex-col justify-between h-screen fixed w-fit pt-[5rem] pb-4 border-base-200 border font-work">
       <div className="flex flex-col">
-        <SideBarItem name="Register a staff" Icon={CgUserAdd} />
+        <SideBarItem name="Add a staff" Icon={CgUserAdd} />
         <SideBarItem name="Attendance" Icon={BiUserCheck} />
         <SideBarItem name="Staff list" Icon={CgUserList} />
         <SideBarItem name="Logs" Icon={VscNote} />
@@ -26,9 +27,19 @@ const AdminSideBar = () => {
 
 const SideBarItem = ({ name, Icon }: SideBarItemProps) => {
   return (
-    <div className="flex flex-col sm:flex-row gap-2 items-center w-full sm:min-w-[10rem] p-3 cursor-pointer hover:bg-primary hover:text-zinc-100 border-transparent rounded">
+    <div
+      className="flex flex-col sm:flex-row gap-2 items-center 
+    w-full sm:min-w-[13rem] sm:px-3 px-1 py-3 cursor-pointer 
+    hover:bg-base-200 border-l-[6px] hover:border-l-primary border-l-base-100 
+    transition-all ease-in-out duration-100"
+    >
       <Icon className="w-5 h-5" />
-      <span className="text-[0.6rem] sm:text-sm">{name}</span>
+      <Link
+        to="/admin/register-staff"
+        className="text-[0.6rem] sm:text-sm lg:text-base text-center"
+      >
+        {name}
+      </Link>
     </div>
   );
 };
