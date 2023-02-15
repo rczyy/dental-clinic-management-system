4786af2394adab4344f77fc3e62d9d7dfd3a21c5
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2023-02-15 15:50:00 +0800

    Changed blob from img to svg

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -3,7 +3,6 @@ import { RiHealthBookLine } from "react-icons/ri";
 import { FaTooth, FaTeethOpen, FaTeeth } from "react-icons/fa";
 import { HiSparkles } from "react-icons/hi";
 import { GiTooth } from "react-icons/gi";
-import Blob from "../assets/blob.svg";
 
 type Props = {};
 
@@ -22,7 +21,7 @@ const Landing = (props: Props) => {
             Book an appointment
           </button>
         </div>
-        <img src={Blob} alt="Blob" className="absolute bottom-0 w-screen" />
+        <Blob className="text-base-300 fill-current" />
       </section>
       <section className="flex flex-col items-center justify-center py-12">
         <h2 className="text-2xl">Services</h2>
@@ -41,6 +40,31 @@ const Landing = (props: Props) => {
         <CategoryBox title="Dentures" Icon={FaTeethOpen} />
         <CategoryBox title="Orthodontics (Braces)" Icon={FaTeeth} />
       </section>
+    </div>
+  );
+};
+
+type BlobProps = {
+  className: string;
+};
+
+const Blob = ({ className }: BlobProps) => {
+  return (
+    <div className="absolute bottom-0 w-screen">
+      <svg
+        id="visual"
+        viewBox="0 0 900 600"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        version="1.1"
+      >
+        <path
+          d="M0 546L37.5 541C75 536 150 526 225 521.3C300 516.7 375 517.3 450 523.7C525 530 600 542 675 548.8C750 555.7 825 557.3 862.5 558.2L900 559L900 601L862.5 601C825 601 750 601 675 601C600 601 525 601 450 601C375 601 300 601 225 601C150 601 75 601 37.5 601L0 601Z"
+          className={className}
+          stroke-linecap="round"
+          stroke-linejoin="miter"
+        ></path>
+      </svg>
     </div>
   );
 };
