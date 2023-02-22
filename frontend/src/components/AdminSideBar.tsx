d6cd0c64a2c79774d8e0dca74764e00392cfe95f
rcmercado81@gmail.com <rczyy>
2023-02-22 16:30:36 +0800

    Added collapse sidebar when clicking on mobile

--- frontend/src/components/AdminSideBar.tsx
+++ frontend/src/components/AdminSideBar.tsx
@@ -18,7 +18,7 @@ const AdminSideBar = () => {
     <div
       className={
         "flex bg-base-100 min-h-[inherit] pt-20 pb-4 px-1 border-base-200 border-r fixed z-20 transition-all md:relative " +
-        (sidebar ? "left-0 w-80" : "-left-80 md:w-0")
+        (sidebar ? "left-0 w-72 md:w-80" : "-left-80 md:w-0")
       }
     >
       <div className="flex flex-col w-full gap-1">
@@ -41,14 +41,17 @@ const AdminSideBar = () => {
 };
 
 const SideBarItem = ({ name, Icon, route }: SideBarItemProps) => {
+  const toggleSidebar = useAdminStore((state) => state.toggleSidebar);
+
   return (
     <Link
       to={route}
-      className="flex gap-2 items-center px-3 py-3 cursor-pointer border-l-[6px] border-l-base-100 
-      hover:border-l-primary hover:bg-base-200 transition-all ease-in-out duration-100"
+      className="flex gap-3.5 items-center px-3 py-4 cursor-pointer border-l-[6px] border-l-base-100 
+      hover:border-l-primary rounded-r-md hover:bg-base-200 transition-all ease-in-out duration-100"
+      onClick={window.innerWidth < 768 ? toggleSidebar : undefined}
     >
       <Icon className="w-6 h-6" />
-      <span className="text-xs sm:text-sm text-center">{name}</span>
+      <span className="text-base text-center font-medium">{name}</span>
     </Link>
   );
 };
