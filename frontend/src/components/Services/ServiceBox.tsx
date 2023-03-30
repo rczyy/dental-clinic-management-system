bb73932885ee7e76efbae7a33099f931ab72c0a2
abdulskie <hussainabdul057@gmail.com>
2023-03-30 15:06:53 +0800

    Fixed height of service box

--- frontend/src/components/Services/ServiceBox.tsx
+++ frontend/src/components/Services/ServiceBox.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 const CategoryBox = ({ title, Icon }: Props) => {
   return (
-    <div className="flex flex-col border border-neutral items-center gap-2 shadow-md p-6 rounded-md">
-      <Icon className="h-24 w-24 text-primary"/>
+    <div className="flex flex-col border border-neutral items-center gap-2 shadow-md  h-[238px] p-6 rounded-md">
+      <Icon className="h-24 w-24 text-primary" />
       <span className="font-semibold text-lg text-center">{title}</span>
       <span className="text-sm">Learn more</span>
     </div>
