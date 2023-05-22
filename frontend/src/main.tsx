e57b4c2327ce7a2f40b6cf7460e2af91dfece0ff
abdulskie <hussainabdul057@gmail.com>
2023-05-22 19:18:57 +0800

    Added Profile Settings Page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,6 @@ import StaffList, { loader as staffLoader } from "./pages/StaffList";
 import PatientList, { loader as patientLoader } from "./pages/PatientList";
 import RegisterStaff from "./pages/RegisterStaff";
 import RegisterPatient from "./pages/RegisterPatient";
-import Services from "./pages/Services";
 import ServiceList from "./pages/ServiceList";
 import AddService from "./pages/AddService";
 import Error from "./pages/Error";
@@ -16,6 +15,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./styles.css";
+import Profile from "./pages/Profile";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -45,6 +45,10 @@ const router = createBrowserRouter([
       {
         path: "signup",
         element: <Signup />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
       },
       {
         path: "dashboard",
@@ -77,10 +81,6 @@ const router = createBrowserRouter([
             element: <AddService />,
           },
         ],
-      },
-      {
-        path: "services",
-        element: <Services />,
       },
     ],
   },
