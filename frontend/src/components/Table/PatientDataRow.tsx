837447cccd1ce2079d40856d5f8428749759b7cf
abdulskie <hussainabdul057@gmail.com>
2023-05-28 13:40:03 +0800

    Added delete feature in View Profile

--- frontend/src/components/Table/PatientDataRow.tsx
+++ frontend/src/components/Table/PatientDataRow.tsx
@@ -1,68 +1,133 @@
-import { FiEdit2, FiEye, FiMoreHorizontal, FiTrash } from "react-icons/fi";
+import { Dispatch, SetStateAction, useState } from "react";
+import { FiEye, FiMoreHorizontal, FiTrash } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import { useRemovePatient } from "../../hooks/patient";
+import { GrFormClose } from "react-icons/gr";
 
 type Props = {
   patient: PatientResponse;
 };
+type RemovePatientProps = {
+  patient: PatientResponse;
+  setIsDeleteModalVisible: Dispatch<SetStateAction<boolean>>;
+};
 
 const PatientDataRow = ({ patient }: Props) => {
+  const navigate = useNavigate();
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
   return (
-    <tr className="[&>*]:bg-transparent transition tracking-tight">
-      <th className="!bg-base-300">
-        <div className="flex dropdown dropdown-right">
-          <label
-            tabIndex={0}
-            className="w-8 h-8 p-2 mx-auto rounded-full cursor-pointer transition hover:bg-base-100"
+    <>
+      <tr className="[&>*]:bg-transparent transition tracking-tight">
+        <th className="!bg-base-300">
+          <div className="flex dropdown dropdown-right">
+            <label
+              tabIndex={0}
+              className="w-8 h-8 p-2 mx-auto rounded-full cursor-pointer transition hover:bg-base-100"
+            >
+              <FiMoreHorizontal />
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu flex-row flex-nowrap p-1 bg-base-100 text-sm border border-neutral rounded-lg shadow-lg translate-x-2 -translate-y-1/4"
+            >
+              <li
+                onClick={() => navigate(`/profile/${patient.user._id}`)}
+              >
+                <a>
+                  <FiEye />
+                </a>
+              </li>
+              <li onClick={() => setIsDeleteModalVisible(true)}>
+                <a>
+                  <FiTrash />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </th>
+        <td className="font-medium text-sm">
+          <div className="flex flex-col">
+            <span>{`${patient.user.name.firstName} ${patient.user.name.lastName}`}</span>
+            <span className="font-medium text-xs text-zinc-400">
+              {patient.user.email}
+            </span>
+          </div>
+        </td>
+        <td className="font-medium text-sm">
+          <div className="flex flex-col">
+            {patient.user.address ? (
+              <>
+                <span>
+                  {`${patient.user.address.street} ${patient.user.address.barangay}`}
+                </span>
+                <span className="font-medium text-xs text-zinc-400">
+                  {`${patient.user.address.city}, ${patient.user.address.province}`}
+                </span>
+              </>
+            ) : (
+              <span className="font-medium text-xs text-zinc-400">—</span>
+            )}
+          </div>
+        </td>
+        <td className="font-medium text-sm">{patient.user.contactNo}</td>
+      </tr>
+      {isDeleteModalVisible && (
+        <RemoveUserModal
+          patient={patient}
+          setIsDeleteModalVisible={setIsDeleteModalVisible}
+        />
+      )}
+    </>
+  );
+};
+
+const RemoveUserModal = ({
+  patient,
+  setIsDeleteModalVisible,
+}: RemovePatientProps) => {
+  const { mutate: removePatient, error: removePatientError } = useRemovePatient(
+    patient.user._id
+  );
+  const handleDelete = () => {
+    removePatient(patient.user._id, {
+      onSuccess: () => setIsDeleteModalVisible(false),
+    });
+  };
+  return (
+    <div className="fixed flex items-center justify-center inset-0 bg-black z-30 bg-opacity-25">
+      <section className="flex flex-col gap-2 bg-base-300 max-w-4xl rounded-2xl shadow-md px-8 py-10">
+        <header className="flex justify-between items-center mx-2 py-3">
+          <h1 className="text-2xl font-bold">Remove Patient</h1>
+          <GrFormClose
+            className="hover: cursor-pointer w-5 h-5"
+            onClick={() => setIsDeleteModalVisible(false)}
+          />
+        </header>
+        <div className="flex flex-col items-center mx-2 py-3">
+          <p>You are about to permanently remove a patient.</p>
+          <p>Are you sure?</p>
+        </div>
+        <div className="flex gap-3 justify-end mx-2 py-3">
+          <button
+            className="btn"
+            onClick={() => setIsDeleteModalVisible(false)}
           >
-            <FiMoreHorizontal />
-          </label>
-          <ul
-            tabIndex={0}
-            className="dropdown-content menu flex-row flex-nowrap p-1 bg-base-100 text-sm border border-neutral rounded-lg shadow-lg translate-x-2 -translate-y-1/4"
+            No
+          </button>
+          <button
+            className="btn btn-error"
+            onClick={() => {
+              handleDelete();
+            }}
           >
-            <li>
-              <a>
-                <FiEye />
-              </a>
-            </li>
-            <li>
-              <a>
-                <FiEdit2 />
-              </a>
-            </li>
-            <li>
-              <a>
-                <FiTrash />
-              </a>
-            </li>
-          </ul>
+            Yes
+          </button>
         </div>
-      </th>
-      <td className="font-medium text-sm">
-        <div className="flex flex-col">
-          <span>{`${patient.user.name.firstName} ${patient.user.name.lastName}`}</span>
-          <span className="font-medium text-xs text-zinc-400">
-            {patient.user.email}
-          </span>
-        </div>
-      </td>
-      <td className="font-medium text-sm">
-        <div className="flex flex-col">
-          {patient.user.address ? (
-            <>
-              <span>
-                {`${patient.user.address.street} ${patient.user.address.barangay}`}
-              </span>
-              <span className="font-medium text-xs text-zinc-400">
-                {`${patient.user.address.city}, ${patient.user.address.province}`}
-              </span>
-            </>
-          ) : (
-            <span className="font-medium text-xs text-zinc-400">—</span>
-          )}
-        </div>
-      </td>
-      <td className="font-medium text-sm">{patient.user.contactNo}</td>
-    </tr>
+        <p className="px-2 text-xs text-error text-center">
+          {removePatientError && removePatientError.response.data.message}
+        </p>
+      </section>
+    </div>
   );
 };
 export default PatientDataRow;
