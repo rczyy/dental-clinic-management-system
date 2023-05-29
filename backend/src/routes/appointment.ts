5360f09ee8a4c5852137c96ea072eb0181c3b27e
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2023-05-29 21:03:09 +0800

    Added remove appointment route

--- backend/src/routes/appointment.ts
+++ backend/src/routes/appointment.ts
@@ -6,6 +6,7 @@ import {
   getAppointments,
   getDentistAppointments,
   getPatientAppointments,
+  removeAppointment,
 } from "../controllers/appointment";
 
 const router = Router();
@@ -16,5 +17,6 @@ router
   .get("/get-patient-appointments/:patient", checkAuth, getPatientAppointments);
 router.post("/add", checkAuth, addAppointment);
 router.put("/edit/:appointmentId", checkAuth, editAppointment);
+router.delete("/remove/:appointmentId", checkAuth, removeAppointment);
 
 export default router;
