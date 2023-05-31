0c6f054019df42abbab8d9e61977c367d36f2199
rczyy <rcmercado81@gmail.com>
2023-05-31 23:00:23 +0800

    Changed proper plural for staff

--- frontend/src/pages/StaffList.tsx
+++ frontend/src/pages/StaffList.tsx
@@ -91,7 +91,7 @@ const StaffList = (props: Props) => {
       } m-auto transition-[max-width]`}
     >
       <header className="flex justify-between items-end mb-4 gap-8">
-        <h1 className="font-bold text-2xl md:text-3xl">Staffs</h1>
+        <h1 className="font-bold text-2xl md:text-3xl">Staff</h1>
         <Link
           to="/staff/register"
           role="button"
