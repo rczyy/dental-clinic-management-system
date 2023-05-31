2de1fb2065ff1e34cbfef78af0b454dbe609dec3
abdulskie <hussainabdul057@gmail.com>
2023-05-31 23:50:44 +0800

    Added filter by date in attendance

--- frontend/src/pages/MyAttendance.tsx
+++ frontend/src/pages/MyAttendance.tsx
@@ -8,13 +8,18 @@ import { useState } from "react";
 import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 import AttendanceDataRow from "../components/Table/AttendanceDataRow";
 import { toast } from "react-toastify";
+import { useAdminStore } from "../store/admin";
+import { DesktopDatePicker } from "@mui/x-date-pickers";
 
 type Props = {};
 const MyAttendance = (props: Props) => {
+  const sidebar = useAdminStore((state) => state.sidebar);
+  const [dateFilter, setDateFilter] = useState<dayjs.Dayjs | null>(null);
   const { mutate: logTimeIn } = useLogTimeIn();
   const { mutate: logTimeOut } = useLogTimeOut();
   const { data } = useGetMyAttendance();
   const [attendanceSort, setAttendanceSort] = useState<"asc" | "desc">();
+
   const filteredAttendance =
     data &&
     data
@@ -32,22 +37,29 @@ const MyAttendance = (props: Props) => {
             : -1
           : 0;
       })
-      .filter((attendance) => attendance.date);
-
+      .filter((attendance) => {
+        if (dateFilter) {
+          return (
+            dayjs(attendance.date).format("DD/MM/YYYY") ===
+            dayjs(dateFilter).format("DD/MM/YYYY")
+          );
+        }
+        return true;
+      });
   const dateToday = new Date().setHours(0, 0, 0, 0);
   const isAlreadyTimedIn =
-    filteredAttendance &&
-    filteredAttendance.find(
+    data &&
+    data.find(
       (attendance) =>
-        dayjs(attendance.date).format() === dayjs(dateToday).format() &&
-        attendance.timeIn
+        dayjs(attendance.timeIn).format("DD/MM/YYYY") ===
+        dayjs(dateToday).format("DD/MM/YYYY")
     );
   const isAlreadyTimedOut =
-    filteredAttendance &&
-    filteredAttendance.find(
+    data &&
+    data.find(
       (attendance) =>
-        dayjs(attendance.date).format() === dayjs(dateToday).format() &&
-        attendance.timeOut
+        dayjs(attendance.timeOut).format("DD/MM/YYYY") ===
+        dayjs(dateToday).format("DD/MM/YYYY")
     );
   const timeInClick = () => {
     logTimeIn(dayjs().format(), {
@@ -60,9 +72,26 @@ const MyAttendance = (props: Props) => {
     });
   };
   return (
-    <main className="flex flex-col gap-4">
+    <main
+      className={`flex flex-col gap-4 ${
+        sidebar ? "max-w-screen-2xl" : "max-w-screen-xl"
+      } mx-auto transition-[max-width]`}
+    >
       <header className="flex justify-between">
-        <h1 className="font-bold text-2xl md:text-3xl">My Attendance</h1>
+        <div className="flex flex-col gap-8">
+          <h1 className="font-bold text-2xl md:text-3xl">My Attendance</h1>
+          <DesktopDatePicker
+            label="Select date"
+            className="w-36 sm:w-44"
+            defaultValue={dateFilter}
+            onChange={(date) => setDateFilter(date)}
+            slotProps={{
+              actionBar: {
+                actions: ["clear"],
+              },
+            }}
+          />
+        </div>
         <div className="flex flex-col gap-1">
           <div className="flex gap-2">
             <button
@@ -88,7 +117,7 @@ const MyAttendance = (props: Props) => {
         <table className="table [&>*]:bg-base-300 w-full text-sm sm:text-base">
           <thead>
             <tr className="[&>*]:bg-base-300 border-b border-base-200">
-              <th
+              <td
                 className="text-primary normal-case cursor-pointer"
                 onClick={() =>
                   setAttendanceSort((val) => (val === "asc" ? "desc" : "asc"))
@@ -102,20 +131,27 @@ const MyAttendance = (props: Props) => {
                     <AiFillCaretUp className="w-2.5 h-2.5" />
                   ) : null}
                 </div>
-              </th>
+              </td>
               <th className="text-primary normal-case text-center">Time In</th>
               <th className="text-primary normal-case text-center">Time Out</th>
             </tr>
           </thead>
           <tbody>
-            {filteredAttendance &&
+            {filteredAttendance && filteredAttendance.length > 0 ? (
               filteredAttendance.map((attendance) => (
                 <AttendanceDataRow
                   key={attendance._id}
                   attendance={attendance}
                   showAllDetails={false}
                 />
-              ))}
+              ))
+            ) : (
+              <tr className="[&>*]:bg-transparent">
+                <td colSpan={5} className="py-8 text-2xl text-center font-bold">
+                  No appointments to show
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
--- frontend/src/pages/StaffAttendance.tsx
+++ frontend/src/pages/StaffAttendance.tsx
@@ -2,40 +2,74 @@ import { useState } from "react";
 import { useGetAttendance } from "../hooks/attendance";
 import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 import AttendanceDataRow from "../components/Table/AttendanceDataRow";
+import { useAdminStore } from "../store/admin";
+import dayjs from "dayjs";
+import { DesktopDatePicker } from "@mui/x-date-pickers";
 
 type Props = {};
 const StaffAttendance = (props: Props) => {
+  const sidebar = useAdminStore((state) => state.sidebar);
   const { data } = useGetAttendance();
+  const [dateFilter, setDateFilter] = useState<dayjs.Dayjs | null>(null);
   const [attendanceSort, setAttendanceSort] = useState<"asc" | "desc">();
   const [nameSort, setNameSort] = useState<"asc" | "desc">();
   const filteredAttendance =
     data &&
     data
       .sort((a, b) => {
-        const nameA = a.date.toString();
-        const nameB = b.date.toString();
+        const attendanceA = a.date.toString();
+        const attendanceB = b.date.toString();
 
         return attendanceSort === "asc"
-          ? nameA < nameB
+          ? attendanceA < attendanceB
             ? -1
             : 1
           : attendanceSort === "desc"
-          ? nameA < nameB
+          ? attendanceA < attendanceB
             ? 1
             : -1
           : 0;
       })
-      .filter((attendance) => attendance.date);
+      .filter((attendance) => {
+        if (dateFilter) {
+          return (
+            dayjs(attendance.date).format("DD/MM/YYYY") ===
+            dayjs(dateFilter).format("DD/MM/YYYY")
+          );
+        }
+        return true;
+      });
   return (
-    <div className="flex flex-col gap-4">
+    <main
+      className={`flex flex-col gap-4 ${
+        sidebar ? "max-w-screen-2xl" : "max-w-screen-xl"
+      } mx-auto transition-[max-width]`}
+    >
       <header className="flex justify-between">
-        <h1 className="font-bold text-2xl md:text-3xl">Staff Attendance</h1>
+      <div className="flex flex-col gap-8">
+          <h1 className="font-bold text-2xl md:text-3xl">Staff Attendance</h1>
+          <DesktopDatePicker
+            label="Select date"
+            className="w-36 sm:w-44"
+            defaultValue={dateFilter}
+            onChange={(date) => setDateFilter(date)}
+            slotProps={{
+              actionBar: {
+                actions: ["clear"],
+              },
+            }}
+          />
+        </div>
       </header>
       <div className="flex justify-end items-center"></div>
       <div className="bg-base-300 py-4 pr-4 rounded-box overflow-x-auto">
         <table className="table [&>*]:bg-base-300 w-full text-sm sm:text-base">
           <thead>
             <tr className="[&>*]:bg-base-300 border-b border-base-200">
+            {filteredAttendance && filteredAttendance.length > 0 && (
+                <th className="min-w-[2.5rem] w-10"></th>
+              )}
+
               <th></th>
               <th
                 className="text-primary normal-case cursor-pointer"
@@ -43,7 +77,7 @@ const StaffAttendance = (props: Props) => {
                   setNameSort((val) => (val === "asc" ? "desc" : "asc"))
                 }
               >
-                <div className="flex items-center gap-1">
+                <div className="flex items-center justify-center gap-1">
                   <span>Name</span>
                   {nameSort === "asc" ? (
                     <AiFillCaretDown className="w-2.5 h-2.5" />
@@ -58,7 +92,7 @@ const StaffAttendance = (props: Props) => {
                   setAttendanceSort((val) => (val === "asc" ? "desc" : "asc"))
                 }
               >
-                <div className="flex items-center gap-1">
+                <div className="flex items-center justify-center gap-1">
                   <span>Date</span>
                   {attendanceSort === "asc" ? (
                     <AiFillCaretDown className="w-2.5 h-2.5" />
@@ -67,26 +101,30 @@ const StaffAttendance = (props: Props) => {
                   ) : null}
                 </div>
               </th>
-              <th className="text-primary normal-case">Time In</th>
-              <th className="text-primary normal-case">Time Out</th>
+              <th className="text-primary normal-case text-center">Time In</th>
+              <th className="text-primary normal-case text-center">Time Out</th>
             </tr>
           </thead>
           <tbody>
-            {filteredAttendance &&
+          {filteredAttendance && filteredAttendance.length > 0 ? (
               filteredAttendance.map((attendance) => (
                 <AttendanceDataRow
                   key={attendance._id}
                   attendance={attendance}
                   showAllDetails={true}
                 />
-              ))}
+              ))
+            ) : (
+              <tr className="[&>*]:bg-transparent">
+                <td colSpan={5} className="py-8 text-2xl text-center font-bold">
+                  No appointments to show
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
-    </div>
+    </main>
   );
 };
-const editStaffAttendance = () => {
-  
-}
 export default StaffAttendance;
