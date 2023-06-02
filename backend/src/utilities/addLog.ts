c399ab5c09b842ac2ea6d90141ae08638a988e5f
trjuanbenjaminongkiko <juan.ongkiko@tooltwist.com>
2023-06-02 12:33:26 +0800

    Added service logs action description

--- backend/src/utilities/addLog.ts
+++ backend/src/utilities/addLog.ts
@@ -1,90 +1,126 @@
-import { z } from "zod";
 import { LogModule, LogType } from "../constants";
-import { isValidObjectId } from "mongoose";
 import { Types } from "mongoose";
 import Log from "../models/log";
 import dayjs from "dayjs";
 import User from "../models/user";
-import Appointment from "../models/appointment";
 import Patient from "../models/patient";
 import Dentist from "../models/dentist";
-import Attendance from "../models/Attendance";
-import Service from "../models/Service";
-import DentistSchedule from "../models/dentistSchedule";
 
 // LogModule ["USER", "APPOINTMENT", "DENTIST'S SCHEDULE", "ATTENDANCE", "SERVICE"]
 // LogType ["CREATE", "UPDATE", "DELETE"]
+
+type StaffPayload = {
+  staff: {
+    user: {
+      name: {
+        firstName: string;
+        middleName: string;
+        lastName: string;
+      };
+      email: string;
+    };
+  };
+};
+
+type UserPayload = {
+  user: {
+    name: {
+      firstName: string;
+      middleName: string;
+      lastName: string;
+    };
+    email: string;
+  };
+};
+
+type PatientPayload = {
+  name: {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+  };
+  email: string;
+};
 
 export const addLog = async (
   user: Types.ObjectId | string,
   module: string,
   type: string,
-  payload:
-    | Types.ObjectId
-    | string
-    | Appointment
-    | DentistSchedule[]
-    | Attendance
-    | Service
-    | User,
+  payload: object | string | Types.ObjectId,
   role = "User"
 ) => {
-  const userEmail = await User.findById(user).select("email");
+  const prefix = `${
+    type.charAt(0).toUpperCase() + type.slice(1).toLowerCase()
+  }d`;
   let action = "";
 
   if (module === LogModule[0]) {
-    action = `${
-      type.charAt(0).toUpperCase() + type.slice(1).toLowerCase()
-    }d a ${role}: ${payload.name.firstName} ${payload.name.lastName} ${
-      payload.email
+    action = `${prefix} a ${role}: ${
+      (payload as PatientPayload).name.firstName
+    } ${(payload as PatientPayload).name.lastName} ${
+      (payload as PatientPayload).email
     }`;
   } else if (module === LogModule[1]) {
-    const patient = await Patient.findById(payload.patient).populate({
+    const patient = await Patient.findById(
+      (payload as Appointment).patient
+    ).populate({
       path: "user",
       select: "name email"
     });
-    const dentist = await Dentist.findById(payload.dentist).populate({
+    const dentist = await Dentist.findById(
+      (payload as Appointment).dentist
+    ).populate({
       path: "staff",
       populate: { path: "user", select: "name email" }
     });
 
     if (!patient || !dentist) return;
-    action = `${type.charAt(0).toUpperCase() + type.slice(1).toLowerCase()}d ${
-      module.charAt(0).toUpperCase() + module.slice(1).toLowerCase()
-    }: Patient: ${patient.user.name.firstName} ${patient.user.name.lastName} ${
-      patient.user.email
-    } Dentist: ${dentist.staff.user.name.firstName} ${
-      dentist.staff.user.name.lastName
-    } ${dentist.staff.user.email} Schedule: ${dayjs(
-      payload.dateTimeScheduled
-    ).format("MMM/DD/YY h:mm A")}`;
+
+    action = `${prefix} an Appointment: Patient: ${
+      (patient as unknown as UserPayload).user.name.firstName
+    } ${(patient as unknown as UserPayload).user.name.lastName} ${
+      (patient as unknown as UserPayload).user.email
+    } Dentist: ${
+      (dentist as unknown as StaffPayload).staff.user.name.firstName
+    } ${(dentist as unknown as StaffPayload).staff.user.name.lastName} ${
+      (dentist as unknown as StaffPayload).staff.user.email
+    } Schedule: ${dayjs((payload as Appointment).dateTimeScheduled).format(
+      "MMM/DD/YY h:mm A"
+    )}`;
   } else if (module === LogModule[2]) {
     const dentist = await Dentist.findById(payload).populate({
       path: "staff",
       populate: { path: "user", select: "name email" }
     });
-    action = `${type.charAt(0).toUpperCase() + type.slice(1).toLowerCase()}d ${
-        module.charAt(0).toUpperCase() + module.slice(1).toLowerCase()
-      }: ${dentist.staff.user.name.firstName} ${dentist.staff.user.name.lastName} ${dentist.staff.user.email}`
+    action = `${prefix} Dentist's schedule: ${
+      (dentist as unknown as StaffPayload).staff.user.name.firstName
+    } ${(dentist as unknown as StaffPayload).staff.user.name.lastName} ${
+      (dentist as unknown as StaffPayload).staff.user.email
+    }`;
   } else if (module === LogModule[3]) {
   } else if (module === LogModule[4]) {
+    action = `${prefix} a Service: Name: ${
+      (payload as Service).name
+    } Estimated time: ${(payload as Service).estimatedTime} Category: ${
+      (payload as Service).category
+    }`;
   }
 
   console.log({
     date: dayjs(),
     module,
-    user: userEmail && userEmail.email,
+    user,
     type,
     action
   });
 
-  //   const logToAdd = new Log({
-  //     date: dayjs(),
-  //     module,
-  //     user: userEmail,
-  //     type,
-  //     action: JSON.stringify(payload)
-  //   });
+    const logToAdd = new Log({
+      date: dayjs(),
+      module,
+      user,
+      type,
+      action
+    });
 
-  //   await logToAdd.save();
+    await logToAdd.save();
 };
