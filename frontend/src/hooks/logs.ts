e41f86bd581fb7e3d09fab4bcd6fdb0859856862
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2023-06-04 23:40:57 +0800

    Changed get logs from react query to rtk query

--- frontend/src/hooks/logs.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getLogs } from "../axios/logs";
-
-export const useGetLogs = () => {
-  return useQuery({
-    queryKey: ["logs"],
-    queryFn: getLogs
-  });
-};
