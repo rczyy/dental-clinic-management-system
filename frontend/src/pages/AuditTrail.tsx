421dc002d557185973ccf3464f78008d1ce8af00
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2023-06-04 23:41:15 +0800

    Integrated logs

--- frontend/src/pages/AuditTrail.tsx
+++ frontend/src/pages/AuditTrail.tsx
@@ -1,12 +1,23 @@
-import AuditTrailDataRow from "../components/Table/AuditTrailDataRow";
-import { useGetLogs } from "../hooks/logs";
+import { useEffect, useState } from "react";
+import { useLazyGetLogsQuery } from "../redux/api/logs";
 import { useAdminStore } from "../store/admin";
+import { DesktopDatePicker } from "@mui/x-date-pickers";
+import dayjs, { Dayjs } from "dayjs";
+import AuditTrailDataRow from "../components/Table/AuditTrailDataRow";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 type Props = {};
 
 const AuditTrail = (props: Props) => {
   const sidebar = useAdminStore((state) => state.sidebar);
-  const { data: logData } = useGetLogs();
+  const [getLogs, { data: logData, isFetching }] = useLazyGetLogsQuery();
+  const [datePickerValue, setDatePickerValue] = useState<Dayjs | null>(dayjs());
+
+  useEffect(() => {
+    datePickerValue
+      ? getLogs(datePickerValue.format("YYYY-MM-DD"))
+      : getLogs("");
+  }, [datePickerValue]);
 
   return (
     <main
@@ -17,6 +28,21 @@ const AuditTrail = (props: Props) => {
       <header className="text-2xl md:text-3xl font-bold">
         <h1>Logs</h1>
       </header>
+      <div className="flex flex-wrap justify-between items-center gap-2">
+        <div className="flex gap-6">
+          <DesktopDatePicker
+            label="Select date"
+            className="w-36 sm:w-44"
+            slotProps={{
+              actionBar: {
+                actions: ["clear"]
+              }
+            }}
+            defaultValue={dayjs()}
+            onChange={(date: Dayjs | null) => setDatePickerValue(date)}
+          />
+        </div>
+      </div>
       <div className="overflow-x-auto my-4">
         <table className="table [&>*]:bg-base-300 w-full text-sm sm:text-base">
           <thead>
@@ -30,12 +56,25 @@ const AuditTrail = (props: Props) => {
             </tr>
           </thead>
           <tbody>
-            {logData && logData.length > 0 ? (
-              logData.map((log) => <AuditTrailDataRow logData={log} key={log._id} />)
+            {!isFetching ? (
+              logData && logData.length > 0 ? (
+                logData.map((log) => (
+                  <AuditTrailDataRow logData={log} key={log._id} />
+                ))
+              ) : (
+                <tr className="[&>*]:bg-transparent">
+                  <td
+                    colSpan={5}
+                    className="py-8 text-2xl text-center font-bold"
+                  >
+                    No logs to show
+                  </td>
+                </tr>
+              )
             ) : (
               <tr className="[&>*]:bg-transparent">
-                <td colSpan={5} className="py-8 text-2xl text-center font-bold">
-                  No logs to show
+                <td colSpan={5} className="py-8">
+                  <AiOutlineLoading3Quarters className="animate-spin w-full"/>
                 </td>
               </tr>
             )}
