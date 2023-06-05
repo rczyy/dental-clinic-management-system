a246154242569927cb3feea6100055bbc33d6652
trryanmercado <ryan.mercado@tooltwist.com>
2023-06-05 15:27:11 +0800

    Added getDeletedBills route

--- backend/src/routes/bill.ts
+++ backend/src/routes/bill.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { checkAuth } from "../middlewares/checkAuth";
-import { addBill, editBill, getBills, removeBill } from "../controllers/bill";
+import {
+  addBill,
+  editBill,
+  getBills,
+  getDeletedBills,
+  removeBill,
+} from "../controllers/bill";
 
 const router = Router();
 
-router.get("/", checkAuth, getBills);
+router
+  .get("/", checkAuth, getBills)
+  .get("/deleted", checkAuth, getDeletedBills);
 router.post("/", checkAuth, addBill);
 router.put("/:billId", checkAuth, editBill);
 router.delete("/:billId", checkAuth, removeBill);
