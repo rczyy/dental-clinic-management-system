ba5bf62c2fd14ac43f0ce1f5c07e25717074b4d1
abdulskie <hussainabdul057@gmail.com>
2023-06-06 22:21:11 +0800

    Improved UI of dashboard

--- frontend/src/components/Dashboard/Card.tsx
+++ frontend/src/components/Dashboard/Card.tsx
@@ -2,18 +2,23 @@ import dayjs from "dayjs";
 import { useGetAttendanceToday } from "../../hooks/attendance";
 import { useGetDentistSchedule } from "../../hooks/dentistSchedule";
 import { useGetAppointments } from "../../hooks/appointment";
-
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { BiMinus, BiPlus } from "react-icons/bi";
+import { useState } from "react";
 type Props = {
   title: string;
 };
 const Card = ({ title }: Props) => {
   const dateToday = dayjs("2023-06-05", "YYYY-MM-DD").format("YYYY-MM-DD");
-  const { data: appointmentData } = useGetAppointments({
-    date: dateToday,
-    includePast: false,
-  });
-  const { data: attendanceToday } = useGetAttendanceToday();
-  const { data: dentistSchedules } = useGetDentistSchedule();
+  const { data: appointmentData, isFetching: appointmentIsFetching } =
+    useGetAppointments({
+      date: dateToday,
+      includePast: false,
+    });
+  const { data: attendanceToday, isFetching: attendanceTodayIsFetching } =
+    useGetAttendanceToday();
+  const { data: dentistSchedules, isFetching: dentistScheduleIsFetching } =
+    useGetDentistSchedule();
   const availableDentists =
     dentistSchedules &&
     dentistSchedules
@@ -31,70 +36,129 @@ const Card = ({ title }: Props) => {
             ),
         };
       });
+  const [bodyDisplay, setBodyDisplay] = useState(true);
   return (
-    <div className="h-80 md:h-96 bg-base-300">
-      <div className="bg-primary py-2 px-4 text-center rounded-t text-xl font-medium">
-        {title}
-      </div>
-      {title === "Appointments Today" && (
-        <div className="flex flex-col gap-4 p-4">
-          {appointmentData && appointmentData.length > 0 ? (
-            <>
-              <div className="flex text-center font-medium">
-                <span className="flex-1">Time</span>
-                <span className="flex-1">Service</span>
-                <span className="flex-1">Patient</span>
-              </div>
-              {appointmentData.map((appointment) => (
-                <div
-                  key={appointment._id}
-                  className="flex break-words text-center items-center text-xs md:text-sm"
-                >
-                  <span className="flex-1">
-                    {dayjs(appointment.dateTimeScheduled).format("h:mm:ss A")}
-                  </span>
-                  <span className="flex-1">{appointment.service.name}</span>
-                  <span className="flex-1">
-                    {appointment.patient.user.name.firstName}{" "}
-                    {appointment.patient.user.name.lastName}
-                  </span>
-                </div>
-              ))}
-            </>
-          ) : (
-            <span className="self-center">No Appointments today</span>
-          )}
+    <div
+      className={
+        "max-h-48 overflow-y-auto shadow-sm " && title === "Available Dentists"
+          ? "md:flex-1"
+          : "md:flex-[2]"
+      }
+    >
+      <div className="flex bg-primary items-center justify-between py-2 px-4 text-center rounded-t text-l font-medium">
+        <span>{title}</span>
+        <div
+          className="hover:cursor-pointer"
+          onClick={() => setBodyDisplay(!bodyDisplay)}
+        >
+          {bodyDisplay ? <BiMinus /> : <BiPlus />}
         </div>
-      )}
-      {title === "Available Dentists" && (
-        <div className="flex flex-col gap-2 p-4">
-          {availableDentists && availableDentists.length > 0 ? (
-            <>
-              <div className="flex text-center font-medium">
-                <span className="flex-1">Name</span>
-                <span className="flex-1">Time In</span>
-              </div>
-              {availableDentists.map((dentist) => (
-                <div
-                  key={dentist.dentist._id}
-                  className="flex break-words items-center text-xs md:text-sm"
-                >
-                  <span className="flex-1 w-[50%] px-2">
-                    {dentist.dentist.staff.user.name.firstName}
-                    {"asdasdasdasdasd "}
-                    {dentist.dentist.staff.user.name.lastName}
-                  </span>
-                  <span className="flex-1 text-center">
-                    {dentist.timeIn !== undefined
-                      ? dayjs(dentist.timeIn.timeIn).format("h:mm:ss A")
-                      : "Not timed in yet"}
-                  </span>
-                </div>
-              ))}
-            </>
-          ) : (
-            <span className="self-center">No dentists available today</span>
-          )}
+      </div>
+      {bodyDisplay && (
+        <div className="max-h-80 overflow-auto">
+          <table className="table [&>*]:bg-base-300 w-full text-sm sm:text-base">
+            {title === "Available Dentists" && (
+              <>
+                <thead>
+                  <tr className="[&>*]:bg-base-300 border-b border-base-200">
+                    <th className="hidden" />
+                    <th className="text-primary normal-case">Dentist</th>
+                    <th className="text-primary normal-case">Time In</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {availableDentists && availableDentists.length > 0 ? (
+                    availableDentists.map((dentist) => (
+                      <tr
+                        key={dentist.dentist._id}
+                        className="[&>*]:bg-base-300 [&>*]:hover:bg-base-100 hover:bg-base-100 [&>*]:py-2 [&>*]:px-4 tracking-tight border-b border-base-200 text-xs md:text-sm cursor-pointer"
+                      >
+                        <td>
+                          {dentist.dentist.staff.user.name.firstName}{" "}
+                          {dentist.dentist.staff.user.name.lastName}
+                        </td>
+                        <td>
+                          {dentist.timeIn
+                            ? `${dayjs(dentist.timeIn.timeIn).format(
+                                "h:mm:ss A"
+                              )}`
+                            : "Not timed in yet"}
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr className="[&>*]:bg-transparent">
+                      {attendanceTodayIsFetching &&
+                      dentistScheduleIsFetching ? (
+                        <td colSpan={5} className="py-8">
+                          <AiOutlineLoading3Quarters className="animate-spin w-full" />
+                        </td>
+                      ) : (
+                        <td
+                          colSpan={5}
+                          className="py-8 text-2xl text-center font-bold"
+                        >
+                          No dentists available today
+                        </td>
+                      )}
+                    </tr>
+                  )}
+                </tbody>
+              </>
+            )}
+            {title === "Appointments Set Today" && (
+              <>
+                <thead>
+                  <tr className="[&>*]:bg-base-300 border-b border-base-200">
+                    <th className="hidden" />
+                    <th className="text-primary normal-case">Time Scheduled</th>
+                    <th className="text-primary normal-case">Service</th>
+                    <th className="text-primary normal-case">Patient</th>
+                    <th className="text-primary normal-case">Dentist</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {appointmentData && appointmentData.length > 0 ? (
+                    appointmentData.map((appointment) => (
+                      <tr
+                        key={appointment._id}
+                        className="[&>*]:bg-base-300 [&>*]:hover:bg-base-100 hover:bg-base-100 [&>*]:py-2 [&>*]:px-4 tracking-tight border-b border-base-200 text-xs md:text-sm cursor-pointer"
+                      >
+                        <td>
+                          {appointment.dateTimeScheduled &&
+                            `${dayjs(appointment.dateTimeScheduled).format("h:mm:ss A")}`}
+                        </td>
+                        <td>{appointment.service.name}</td>
+                        <td>
+                          {appointment.patient.user.name.firstName}{" "}
+                          {appointment.patient.user.name.lastName}
+                        </td>
+                        <td>
+                          {appointment.dentist.staff.user.name.firstName}{" "}
+                          {appointment.dentist.staff.user.name.lastName}
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr className="[&>*]:bg-transparent">
+                      {appointmentIsFetching ? (
+                        <td colSpan={5} className="py-8">
+                          <AiOutlineLoading3Quarters className="animate-spin w-full" />
+                        </td>
+                      ) : (
+                        <td
+                          colSpan={5}
+                          className="py-8 text-2xl text-center font-bold"
+                        >
+                          No appointments set today
+                        </td>
+                      )}
+                    </tr>
+                  )}
+                </tbody>
+              </>
+            )}
+          </table>
         </div>
       )}
     </div>
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,29 +1,107 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import Card from "../components/Dashboard/Card";
 import Stat from "../components/Dashboard/Stat";
+import { LogModal } from "../components/Modal/LogModal";
+import AuditTrailDataRow from "../components/Table/AuditTrailDataRow";
 import { useAdminStore } from "../store/admin";
+import { useLazyGetLogsQuery } from "../redux/api/logs";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { BiMinus, BiPlus } from "react-icons/bi";
 
 type Props = {};
 const Dashboard = (props: Props) => {
   const sidebar = useAdminStore((state) => state.sidebar);
   return (
-    <main
-      className={`flex flex-col gap-4 ${
-        sidebar ? "max-w-screen-2xl" : "max-w-screen-xl"
-      } mx-auto`}
-    >
-      <header className="flex flex-col w-full md:flex-row justify-between mb-4 gap-8">
-        <h1 className="text-2xl md:text-3xl font-bold">Dashboard</h1>
-        <div className="flex flex-col gap-4 md:flex-row">
-          <Stat title="Patients Registered"/>
-          <Stat title="Staffs Registered" />
+    <>
+      <main
+        className={`flex flex-col gap-4 ${
+          sidebar ? "max-w-screen-2xl" : "max-w-screen-xl"
+        } mx-auto`}
+      >
+        <header className="flex flex-col w-full justify-between mb-4 gap-8">
+          <h1 className="text-2xl md:text-3xl font-bold">Dashboard</h1>
+          <div>
+            <div className="flex flex-col gap-4 md:flex-row">
+              <Stat title="Patients Registered" />
+              <Stat title="Staffs Registered" />
+              <Stat title="Total Services Offered" />
+              <Stat title="Appointments for the Week" />
+            </div>
+          </div>
+        </header>
+        <div className="flex flex-col text-md gap-4">
+          <RecentActivity />
+          <div className="flex flex-col gap-4 md:flex-row">
+            <Card title="Available Dentists" />
+            <Card title="Appointments Set Today" />
+          </div>
         </div>
-      </header>
-      <div className="flex flex-col text-md gap-4 md:grid md:grid-cols-2 lg:grid-cols-3">
-        <Card title="Appointments Today" />
-        <Card title="Available Dentists" />
-        <Card title="Billings Today" />
+      </main>
+    </>
+  );
+};
+
+const RecentActivity = () => {
+  const dateToday = dayjs().format("YYYY-MM-DD");
+  const [getLogs, { data: logData, isFetching }] = useLazyGetLogsQuery();
+  const [bodyDisplay, setBodyDisplay] = useState(true);
+  useEffect(() => {
+    getLogs(dateToday);
+  }, [logData]);
+  return (
+    <>
+      <div className="shadow-sm">
+        <div className="flex items-center justify-between bg-primary py-2 px-4 text-center rounded-t text-l font-medium">
+          Recent Activity
+          <div
+            className="hover:cursor-pointer"
+            onClick={() => setBodyDisplay(!bodyDisplay)}
+          >
+            {bodyDisplay ? <BiMinus /> : <BiPlus />}
+          </div>
+        </div>
+        {bodyDisplay && (
+          <div className="max-h-80 overflow-auto">
+            <table className="table [&>*]:bg-base-300 w-full text-sm sm:text-base">
+              <thead>
+                <tr className="[&>*]:bg-base-300 border-b border-base-200">
+                  <th className="hidden" />
+                  <th className="text-primary normal-case">Date</th>
+                  <th className="text-primary normal-case">Module</th>
+                  <th className="text-primary normal-case">User</th>
+                  <th className="text-primary normal-case">Type</th>
+                  <th className="text-primary normal-case">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {logData && logData.length > 0 ? (
+                  logData.map((log) => (
+                    <AuditTrailDataRow logData={log} key={log._id} />
+                  ))
+                ) : (
+                  <tr className="[&>*]:bg-transparent">
+                    {isFetching ? (
+                      <td colSpan={5} className="py-8">
+                        <AiOutlineLoading3Quarters className="animate-spin w-full" />
+                      </td>
+                    ) : (
+                      <td
+                        colSpan={5}
+                        className="py-8 text-2xl text-center font-bold"
+                      >
+                        No logs to show
+                      </td>
+                    )}
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
-    </main>
+      <LogModal />
+    </>
   );
 };
 export default Dashboard;
