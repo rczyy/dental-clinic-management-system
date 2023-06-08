3e70bf1b322eabb2f323bff126ed5fb5367738dc
rc <rcmercado81@gmail.com>
2023-06-08 20:01:54 +0800

    Removed '/' on end of path in localhost

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,7 +31,7 @@ const main = async () => {
 
   app.use(
     cors({
-      origin: ["http://localhost:5173/", "https://atdentalhome.vercel.app"],
+      origin: ["http://localhost:5173", "https://atdentalhome.vercel.app"],
       credentials: true,
     })
   );
