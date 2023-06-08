c34ffebb5c50f78d4d4f9a3c81c2754df871f73a
rc <rcmercado81@gmail.com>
2023-06-08 20:07:46 +0800

    Fixed patient names

--- backend/src/controllers/patient.ts
+++ backend/src/controllers/patient.ts
@@ -78,7 +78,10 @@ export const getPatientNames: RequestHandler = async (req, res) => {
     return;
   }
 
-  const patients = await Patient.find({ isDeleted: false }).populate({
+  const patients = await Patient.find({
+    isDeleted: false,
+    verified: true,
+  }).populate({
     path: "user",
   });
 
