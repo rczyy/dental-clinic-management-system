d1fa3dbe2a00bd4870df4c641d23f73bd0c445ee
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2023-06-14 23:12:29 +0800

    Added tailwind typography

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require('@tailwindcss/typography')],
   daisyui: {
     themes: [
       {
