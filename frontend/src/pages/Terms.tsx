e186256e2f62e10d5fb0923f6edab6ca7e405daf
jobenongkiko <ongkikojuanbenjamin@gmail.com>
2023-06-14 23:12:41 +0800

    Added terms page

---
+++ frontend/src/pages/Terms.tsx
@@ -0,0 +1,175 @@
+type Props = {};
+const Terms = (props: Props) => {
+  return (
+    <main className="flex justify-center">
+      <div  className="prose prose-sm">
+        <header>
+          <h1>Privacy Policy</h1>
+        </header>
+
+        <section>
+          <h2>Information Collected</h2>
+          <p>
+            When you register an account, we collect the following personal
+            information:
+          </p>
+          <ul>
+            <li>Name</li>
+            <li>Contact number</li>
+            <li>Address</li>
+            <li>Email address</li>
+          </ul>
+          <p>
+            Additionally, our website will collect and store the following
+            information related to your dental appointments:
+          </p>
+          <ul>
+            <li>History of appointments</li>
+            <li>Dental history and any relevant medical records</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2>Use of Information</h2>
+          <p>We use the collected information for the following purposes:</p>
+          <ul>
+            <li>To schedule and manage your dental appointments</li>
+            <li>To provide you with relevant dental services</li>
+            <li>
+              To communicate with you regarding appointment confirmations,
+              reminders, and updates
+            </li>
+            <li>
+              To maintain accurate and up-to-date dental records for your
+              treatment
+            </li>
+            <li>To improve our services and website functionality</li>
+            <li>To comply with legal obligations</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2>Information Security</h2>
+          <p>
+            We take appropriate measures to ensure the security of your personal
+            information. We have implemented technical and
+            administrative safeguards to protect against unauthorized access,
+            alteration, disclosure, or destruction of your personal data.
+          </p>
+        </section>
+
+        <section>
+          <h2>Information Disclosure</h2>
+          <p>
+            We may disclose your personal information in the following
+            circumstances:
+          </p>
+          <ul>
+            <li>
+              To dental professionals and staff involved in your treatment and
+              care
+            </li>
+            <li>
+              To third-party service providers who assist us in operating our
+              website and providing our services
+            </li>
+            <li>
+              To comply with legal obligations or respond to lawful requests
+              from public authorities
+            </li>
+          </ul>
+          <p>
+            We do not sell, trade, or rent your personal information to third
+            parties for marketing purposes.
+          </p>
+        </section>
+
+        <section>
+          <h2>Data Retention</h2>
+          <p>
+            We retain your personal information as long as necessary to fulfill
+            the purposes outlined in this Privacy Policy or as required by law.
+            When your information is no longer needed, we will securely delete
+            or anonymize it.
+          </p>
+        </section>
+
+        <section>
+          <h2>Consent</h2>
+          <p>
+            By using our web-based dental clinic appointment system and
+            providing your personal information, you consent to the collection,
+            use, and disclosure of your information as described in this Privacy
+            Policy.
+          </p>
+        </section>
+
+        <header>
+          <h1>Terms and Conditions</h1>
+        </header>
+
+        <section>
+          <h2>Appointment Cancellation</h2>
+          <p>
+            To cancel an appointment, you must notify us at least 2 days before
+            the scheduled appointment. Failure to provide sufficient notice may
+            result in a penalty fee or loss of future appointment privileges.
+          </p>
+        </section>
+
+        <section>
+          <h2>Appointment Rescheduling</h2>
+          <p>
+            If you arrive late for your scheduled appointment, the clinic
+            reserves the right to reschedule your appointment to a later time or
+            date based on availability. We encourage all patients to arrive on
+            time to ensure prompt and efficient service.
+          </p>
+        </section>
+
+        <section>
+          <h2>Dental History Disclosure</h2>
+          <p>
+            It is important for patients to inform the clinic about their dental
+            history, including any pre-existing conditions, allergies, or
+            medications that may affect their treatment. Providing accurate and
+            complete information is essential for ensuring safe and effective
+            dental care.
+          </p>
+        </section>
+
+        <section>
+          <h2>Website Usage</h2>
+          <p>
+            By using our web-based dental clinic appointment system, you agree
+            to abide by all applicable laws and regulations. You may not use the
+            website for any unlawful purposes or engage in any activity that
+            could damage, disable, or impair the website's functionality.
+          </p>
+        </section>
+
+        <section>
+          <h2>Disclaimer of Liability</h2>
+          <p>
+            While we strive to provide accurate and up-to-date information, we
+            do not guarantee the accuracy, completeness, or reliability of any
+            content on our website. The clinic shall not be held liable for any
+            direct or indirect damages resulting from the use of our website or
+            any actions taken based on the information provided.
+          </p>
+        </section>
+
+        <section>
+          <h2>Modifications to Terms and Conditions</h2>
+          <p>
+            We reserve the right to modify or update these Terms and Conditions
+            at any time. Any changes will be effective immediately upon posting
+            on our website. It is your responsibility to review these Terms and
+            Conditions periodically for any updates.
+          </p>
+        </section>
+      </div>
+    </main>
+  );
+};
+export default Terms;
