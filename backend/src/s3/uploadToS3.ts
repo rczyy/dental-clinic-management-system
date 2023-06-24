f2df63e61c1d8c8bc1b383cb9162f012f1529545
rczyy <rcmercado81@gmail.com>
2023-06-24 16:41:20 +0800

    Fixed aws env access and secret key

--- backend/src/s3/uploadToS3.ts
+++ backend/src/s3/uploadToS3.ts
@@ -3,8 +3,8 @@ import S3 from "aws-sdk/clients/s3";
 export const uploadToS3 = async (key: string, body: S3.Body, contentType: string) => {
   const region = process.env.AWS_REGION;
   const bucket = process.env.AWS_BUCKET;
-  const accessKeyId = process.env.AWS_ACCESSKEY;
-  const secretAccessKey = process.env.AWS_SECRETKEY;
+  const accessKeyId = process.env.AWS_ACCESSKEY || process.env.AWS_ACCESS_KEY;
+  const secretAccessKey = process.env.AWS_SECRETKEY || process.env.AWS_ACCESS_KEY;
 
   if (!region) throw new Error("No AWS region provided");
   if (!bucket) throw new Error("No AWS bucket provided");
